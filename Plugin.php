302842ddc3e50f824b8042e22f0e1f23db80768f
Matthew Brown <github@muglug.com>
2018-11-25 14:04:58 -0500

    Fix bad ref

--- Plugin.php
+++ Plugin.php
@@ -8,9 +8,9 @@ use Psalm\Plugin\RegistrationInterface;
 class Plugin implements PluginEntryPointInterface
 {
     /** @return void */
-    public function __invoke(RegistrationInterface $psalm, SimpleXMLElement $config = null)
+    public function __invoke(RegistrationInterface $registration, SimpleXMLElement $config = null)
     {
-    	$psalm->addStubFile(__DIR__ . '/stubs/Mockery.php');
+    	$registration->addStubFile(__DIR__ . '/stubs/Mockery.php');
     	
     	require_once __DIR__ . '/hooks/MockReturnTypeUpdater.php';
         $registration->registerHooksFromClass(Hooks\MockReturnTypeUpdater::class);
