6411bb72fe165abe1127d47e1c0456a5e7915b6d
Bruce Weirdan <weirdan@gmail.com>
2019-06-29 05:25:36 +0300

    Don't force installation of muglug/package-versions-56
    Use whatever Psalm brings in: either `muglug/package-versions-56` or
    `ocramius/package-versions`

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -4,7 +4,9 @@ use Codeception\Exception\Skip;
 use Codeception\Exception\TestRuntimeException;
 use Composer\Semver\Comparator;
 use Composer\Semver\VersionParser;
-use Muglug\PackageVersions\Versions;
+use Muglug\PackageVersions\Versions as LegacyVersions;
+use PackageVersions\Versions as Versions;
+use RuntimeException;
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 class Acceptance extends \Codeception\Module
@@ -26,7 +28,7 @@ class Acceptance extends \Codeception\Module
             throw new TestRuntimeException("Unknown operator: $operator");
         }
         $op = (string) self::VERSION_OPERATORS[$operator];
-        $currentVersion = (string) Versions::getShortVersion('mockery/mockery');
+        $currentVersion = $this->getShortVersion('mockery/mockery');
         $this->debug(sprintf("Current version: %s", $currentVersion));
         $parser = new VersionParser();
         $currentVersion = $parser->normalize($currentVersion);
@@ -36,5 +38,26 @@ class Acceptance extends \Codeception\Module
         if (!$result) {
             throw new Skip("This scenario requires Mockery $op $version because of $reason");
         }
+    }
+
+    private function getShortVersion(string $package): string
+    {
+        if (class_exists(Versions::class)) {
+            /** @psalm-suppress UndefinedClass psalm 3.0 ignores class_exists check */
+            $version = (string) Versions::getVersion($package);
+        } elseif (class_exists(LegacyVersions::class)) {
+            $version = (string) LegacyVersions::getVersion($package);
+        } else {
+            throw new RuntimeException(
+                'Neither muglug/package-versions-56 nor ocramius/package-version is available,'
+                . ' cannot determine versions'
+            );
+        }
+
+        if (false === strpos($version, '@')) {
+            throw new RuntimeException('$version must contain @');
+        }
+
+        return explode('@', $version)[0];
     }
 }
